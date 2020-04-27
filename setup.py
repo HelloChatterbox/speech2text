a365b0c09c7e5c9966181b00fa8a009cc89a0d65
jarbasal <jarbasai@mailfence.com>
2020-04-27 18:24:02 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='speech2text',
-    version='0.1',
+    version='0.1.0',
     packages=['speech2text', 'speech2text.engines'],
-    url='',
+    url='https://github.com/JarbasAl/speech2text',
     install_requires=["requests", "SpeechRecognition==3.8.1"],
     license='Apache2.0',
     author='jarbasAI',
     author_email='',
-    description=''
+    description='Mycroft STT engine wrappers'
 )
